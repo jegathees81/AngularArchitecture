b11ebe8a135c35c432fc98d36a15386a1c4037fb
jegathees81 <31097441+jegathees81@users.noreply.github.com>
2017-09-14 18:13:21 +0530

    Update app.route.ts

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./Elite.View/home/home.component";
 import { UsersComponent } from "./Elite.View/users/users.component";
-import { TodoComponent } from "./Elite.View/todolist/todo/todo.component";
 
 
 
 const routes: Routes = [
     { path: 'home', pathMatch: 'full', component: HomeComponent },
-    {path:'users', pathMatch:'full', component: UsersComponent },
-    {path:'todo', pathMatch:'full', component: TodoComponent },
+    {path:'users', pathMatch:'full', component: UsersComponent }
+    
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
